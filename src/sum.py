56fac48ec9046494201b9e94a2c96850815f55e5
JP Nicolas <jp@thinkingmachin.es>
2024-08-28 22:36:17 +0800

    Update sum.py

--- src/sum.py
+++ src/sum.py
@@ -2,9 +2,9 @@ import sys
 
 
 def sum(num_a: float, num_b: float) -> None:
-    print(f"num_a:{num_a}")
-    print(f"num_b:{num_b}")
-    print(f"sum:{num_a + num_b}")
+    print(f"num_a: {num_a}")
+    print(f"num_b: {num_b}")
+    print(f"sum: {num_a + num_b}")
 
 
 if __name__ == "__main__":
